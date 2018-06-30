bba5b187b0704f605fcc62d096e6847b6f7a98d8
Alejandro Roman <aroman@carecloud.com>
2018-06-30 17:21:23 -0400

    refactor

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,8 +1,8 @@
 import * as ActionTypes from '../constants/constants';
 // import { API_KEY, URL_DR } from '../env';
 import axios from 'axios';
-export const FETCH_DOCTORS = 'FETCH_DOCTORS';
-export const FETCH_LOCATIONS = 'FETCH_LOCATIONS';
+const FETCH_DOCTORS = 'FETCH_DOCTORS';
+const FETCH_LOCATIONS = 'FETCH_LOCATIONS';
 export const fetchDoctors = (lat = '37.773' , lon = '-122.413') => async dispatch => {
     try {
         const response = await axios.get(`${URL_DR}/doctors?location=${lat},${lon},100&skip=2&limit=12&user_key=${API_KEY}`)
